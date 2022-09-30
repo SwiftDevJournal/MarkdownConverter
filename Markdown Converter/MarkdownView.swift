3583010727e224b025a90960b1c981efce212477
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-30 15:14:18 -0400

    Fill the Markdown view.

--- Markdown Converter/MarkdownView.swift
+++ Markdown Converter/MarkdownView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct MarkdownView: View {
+    @State var markdown: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Markdown")
+                .font(.title)
+            TextEditor(text: $markdown)
+        }
     }
 }
 
