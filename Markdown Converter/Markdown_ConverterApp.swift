26da700b52970f448ca78ab83cde683014252796
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-30 15:06:20 -0400

    Initial Commit

---
+++ Markdown Converter/Markdown_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Markdown_ConverterApp.swift
+//  Markdown Converter
+//
+//  Created by mark on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Markdown_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Markdown ConverterUITests/Markdown_ConverterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Markdown_ConverterUITestsLaunchTests.swift
+//  Markdown ConverterUITests
+//
+//  Created by mark on 9/30/22.
+//
+
+import XCTest
+
+final class Markdown_ConverterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
