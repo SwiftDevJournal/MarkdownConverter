c3d2a5895f80e827eb777b09eeeb92d9b67a3713
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-30 15:09:30 -0400

    Add a Markdown view to the project.

---
+++ Markdown Converter/MarkdownView.swift
@@ -0,0 +1,20 @@
+//
+//  MarkdownView.swift
+//  Markdown Converter
+//
+//  Created by mark on 9/30/22.
+//
+
+import SwiftUI
+
+struct MarkdownView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MarkdownView_Previews: PreviewProvider {
+    static var previews: some View {
+        MarkdownView()
+    }
+}
