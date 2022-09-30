e9933235a5368fbcaf62c9a7544ab96e2c2b29d8
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-30 15:09:59 -0400

    Add an HTML view to the project.

---
+++ Markdown Converter/HTMLView.swift
@@ -0,0 +1,20 @@
+//
+//  HTMLView.swift
+//  Markdown Converter
+//
+//  Created by mark on 9/30/22.
+//
+
+import SwiftUI
+
+struct HTMLView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HTMLView_Previews: PreviewProvider {
+    static var previews: some View {
+        HTMLView()
+    }
+}
