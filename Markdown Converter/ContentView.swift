0f908e1b2b8937b78784b14e2df599f36b61a847
Mark Szymczyk <mark@swiftdevjournal.com>
2022-09-30 15:15:31 -0400

    Create an HStack with the Markdown and HTML views side by side.
    The next step is to do the conversion.

--- Markdown Converter/ContentView.swift
+++ Markdown Converter/ContentView.swift
@@ -9,11 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            MarkdownView()
+            HTMLView()
         }
         .padding()
     }
